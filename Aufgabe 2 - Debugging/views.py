aad8390da18060b0e856568584af8dfc252f5c99
Henrich <hfocht@stud.hs-heilbronn.de>
2023-06-13 13:36:59 +0200

    fix

--- Aufgabe 2 - Debugging/views.py
+++ Aufgabe 2 - Debugging/views.py
@@ -1,7 +1,7 @@
 from logic import sum_number 
 def main():
-    num1 = input("Erste Zahl anzeigen: ")
-    num2 = input("Erste Zahl anzeigen: ")
+    num1 = input("Erste Zahl eingeben: ")
+    num2 = input("Zweite Zahl eingeben: ")
     result = sum_number(num1,num2)
     print(f"Die Summe von {num1} und {num2} = {result}")
 
